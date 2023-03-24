6b3c1c46d65e03880df41489f20d63459257ca56
Nikhil Raj <nikhil25803@gmail.com>
2023-03-24 20:41:05 +0530

    db

--- db/db.py
+++ db/db.py
@@ -2,13 +2,13 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-
 SQLALCHEMY_DATABASE_URl = "sqlite:///./products.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URl,
     connect_args={"check_same_thread":False}
 )
+
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
